da261cf936c3e65c14cc70a6c19e540f70310436
May'n <wangxuchen_uestc@163.com>
2020-12-28 17:26:57 +0800

    Update login.vue

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,11 +1,11 @@
 <template>
 	<div>
-		<el-form ref="form" :model="form" label-width="80px">
+		<el-form ref="form" :model="form" label-width="80px" class="login-box">
 			<el-form-item label="用户名">
-				<el-input v-model="form.name"></el-input>
+				<el-input v-model="form.name" placeholder="请输入用户名"></el-input>
 			</el-form-item>
 			<el-form-item label="密码">
-				<el-input v-model="form.password"></el-input>
+				<el-input v-model="form.password" placeholder="请输入密码"></el-input>
 			</el-form-item>
 			<el-form-item>
 				<el-button type="primary" @click="onSubmit">登录</el-button>
@@ -36,4 +36,8 @@
 </script>
 
 <style>
+	.login-box{
+		width: 400px;
+		margin:  auto;
+	}
 </style>
