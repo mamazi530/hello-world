8b3049169d469d682ccbcb55c0d0b9705f2f3b75
May'n <wangxuchen_uestc@163.com>
2020-12-28 17:18:30 +0800

    init Vue with vue cli 3

---
+++ src/views/login.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<el-form ref="form" :model="form" label-width="80px">
+			<el-form-item label="用户名">
+				<el-input v-model="form.name"></el-input>
+			</el-form-item>
+			<el-form-item label="密码">
+				<el-input v-model="form.password"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="onSubmit">登录</el-button>
+				<el-button>取消</el-button>
+			</el-form-item>
+		</el-form>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "login",
+		data() {
+			return {
+				form: {
+					name: '',
+					password: ''
+				}
+			}
+		},
+		methods: {
+			onSubmit() {
+				console.log("登录" + this.$data.form.name)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
