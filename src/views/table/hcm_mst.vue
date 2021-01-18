440c0cc4e44b2976c080bba9e5e8f70ccd05c884
May'n <wangxuchen_uestc@163.com>
2021-01-18 18:15:40 +0800

    xuchen-mac

--- src/views/table/hcm_mst.vue
+++ src/views/table/hcm_mst.vue
@@ -1,26 +1,73 @@
 <template>
   <div>
-    <el-button @click="query()" type="primary">查询</el-button>
+    <el-button @click="query()" type="primary" icon="el-icon-search"
+      >查询</el-button
+    >
+    <el-button
+      @click="newLine()"
+      type="primary"
+      icon="el-icon-circle-plus-outline"
+      plain
+      >新增</el-button
+    >
+
+    <el-dialog
+      :title="dialogTitle"
+      :visible.sync="dialogFormVisible"
+      v-if="dialogFormVisible"
+    >
+      <el-form :model="form">
+        <el-form-item label="Payroll" label-width="120px">
+          <el-input
+            v-model="form.payroll_element_name"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Use_Status" label-width="120px">
+          <el-select v-model="form.use_status" placeholder="请选择">
+            <el-option label="Y" value="Y"></el-option>
+            <el-option label="N" value="N"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="is_negative_num" label-width="120px">
+          <el-select v-model="form.is_negative_num" placeholder="请选择">
+            <el-option label="正数" value=""></el-option>
+            <el-option label="负数" value="-"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
+
     <el-table style="width: 100%" border :data="tableData">
       <template v-for="(item, index) in tableHead">
         <el-table-column
           :prop="item.column_name"
           :label="item.column_comment"
           :key="index"
+          :width="item.column_width"
           v-if="item.column_name != 'id'"
         ></el-table-column>
       </template>
       <el-table-column align="center" label="操作">
         <template slot-scope="scope">
           <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-            >Edit</el-button
+            >编辑</el-button
           >
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.$index, scope.row)"
-            >Delete</el-button
+
+          <el-popconfirm
+            title="确定删除这条数据？"
+            @confirm="handleDelete(scope.$index, scope.row)"
           >
+            <el-button slot="reference" size="mini" type="danger"
+              >删除</el-button
+            >
+          </el-popconfirm>
         </template>
       </el-table-column>
     </el-table>
@@ -35,24 +82,37 @@ export default {
         {
           column_name: "payroll_element_name",
           column_comment: "payroll",
+          column_width: "250",
         },
         {
           column_name: "use_status",
           column_comment: "use_status",
+          column_width: "100",
         },
         {
           column_name: "is_negative_num",
           column_comment: "is_negative_num",
+          column_width: "150",
         },
       ],
       tableData: [],
+      dialogFormVisible: false,
+      form: {
+        payroll_element_name: null,
+        use_status: null,
+        is_negative_num: null,
+      },
+      dialogTitle: "",
     };
   },
   methods: {
+    deleteConfim(index) {
+      console.log(index);
+    },
     handleEdit(index, row) {
-      this.tableData.forEach((v) => {
-        console.log("---->" + v);
-      });
+      this.dialogFormVisible = true;
+      this.dialogTitle = "编辑";
+      this.form = row;
     },
     handleDelete(index, row) {
       for (var item in row) {
@@ -78,6 +138,11 @@ export default {
             type: "error",
           });
         });
+    },
+
+    newLine() {
+      this.dialogFormVisible = true;
+      this.dialogTitle = "新建";
     },
 
     axiosConfig(method, endpoint, data) {
